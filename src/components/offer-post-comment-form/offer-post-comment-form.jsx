90e0e2dcd8a6641bb358663044d2c4dee5d0b6ca
unknown <kraynevroman@gmail.com>
2020-11-16 01:45:11 +0300

    clear text area after sending comment

--- src/components/offer-post-comment-form/offer-post-comment-form.jsx
+++ src/components/offer-post-comment-form/offer-post-comment-form.jsx
@@ -28,11 +28,15 @@ class PostCommentForm extends React.PureComponent {
       <form className="reviews__form form" action="#" method="post" onSubmit={handelFormSubmit}>
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
-          {ratingStars.map((stars) => <RatingStar key={stars} index={stars} onChange={handleRatingAreaChange}/>)}
+          {ratingStars.map((stars) => <RatingStar key={`${stars}${rating}`} index={stars} onChange={handleRatingAreaChange}/>)}
         </div>
-        <textarea className="reviews__textarea form__textarea" id="review" name="review"
+        <textarea
+          className="reviews__textarea form__textarea" id="review" name="review"
           placeholder="Tell how was your stay, what you like and what can be improved"
-          onChange={handleTextAreaChange}></textarea>
+          onChange={handleTextAreaChange}
+          value={text}
+        >
+        </textarea>
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
                       To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
