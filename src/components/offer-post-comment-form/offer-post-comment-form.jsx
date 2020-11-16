96e8f474581ef057903c80dd7b005d56e4ccae8f
unknown <kraynevroman@gmail.com>
2020-11-16 14:12:37 +0300

    fix little things

--- src/components/offer-post-comment-form/offer-post-comment-form.jsx
+++ src/components/offer-post-comment-form/offer-post-comment-form.jsx
@@ -6,47 +6,46 @@ import {ActionCreator} from "../../store/action";
 import {ApiActionCreator} from "../../store/api-actions";
 import RatingStar from "../offer-post-comment-rating-star/offer-post-comment-rating-star";
 
-class PostCommentForm extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const PostCommentForm = ({id, rating, text, postComment, setRating, enterText}) => {
+  const handelFormSubmit = (evt) => {
+    evt.preventDefault();
+    postComment(id, {comment: text, rating});
+  };
 
-  render() {
-    const {id, rating, text, postComment, setRating, enterText} = this.props;
-    const handelFormSubmit = (evt) => {
-      evt.preventDefault();
-      postComment(id, {comment: text, rating});
-    };
-
-    const handleRatingAreaChange = (stars) => {
-      setRating(stars);
-    };
-    const handleTextAreaChange = (evt) => {
-      enterText(evt.target.value);
-    };
-    return (
-      <form className="reviews__form form" action="#" method="post" onSubmit={handelFormSubmit}>
-        <label className="reviews__label form__label" htmlFor="review">Your review</label>
-        <div className="reviews__rating-form form__rating">
-          {ratingStars.map((stars) => <RatingStar key={stars} index={stars} onChange={handleRatingAreaChange}/>)}
-        </div>
-        <textarea
-          className="reviews__textarea form__textarea" id="review" name="review"
-          placeholder="Tell how was your stay, what you like and what can be improved"
-          onChange={handleTextAreaChange}
-          value={text}
-        >
-        </textarea>
-        <div className="reviews__button-wrapper">
-          <p className="reviews__help">
+  const handleRatingAreaChange = (stars) => {
+    setRating(stars);
+  };
+  const handleTextAreaChange = (evt) => {
+    enterText(evt.target.value);
+  };
+  return (
+    <form className="reviews__form form" action="#" method="post" onSubmit={handelFormSubmit}>
+      <label className="reviews__label form__label" htmlFor="review">Your review</label>
+      <div className="reviews__rating-form form__rating">
+        {ratingStars.map((star) => <RatingStar
+          key={star}
+          rating={star}
+          currentRating={rating}
+          onChange={handleRatingAreaChange}
+        />)}
+      </div>
+      <textarea
+        className="reviews__textarea form__textarea" id="review" name="review"
+        placeholder="Tell how was your stay, what you like and what can be improved"
+        onChange={handleTextAreaChange}
+        value={text}
+      >
+      </textarea>
+      <div className="reviews__button-wrapper">
+        <p className="reviews__help">
                       To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
-          </p>
-          <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
-        </div>
-      </form>
-    );
-  }
-}
+        </p>
+        <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
+      </div>
+    </form>
+  );
+};
+
 
 PostCommentForm.propTypes = {
   id: PropTypes.number.isRequired,
--- src/components/offer-post-comment-rating-star/offer-post-comment-rating-star.jsx
+++ src/components/offer-post-comment-rating-star/offer-post-comment-rating-star.jsx
@@ -2,18 +2,23 @@ import {PropTypes} from "prop-types";
 import React, {Fragment} from "react";
 import {ratingInputTitle} from "../../constants";
 
-const RatingStar = (props) => {
-  const {index, onChange} = props;
+const RatingStar = ({rating, currentRating, onChange}) => {
   const handleInputChange = (evt) =>{
     onChange(evt.target.value);
   };
 
   return (
     <Fragment>
-      <input className="form__rating-input visually-hidden" name="rating" value={index} id={`${index}-stars`} type="radio"
+      <input
+        className="form__rating-input visually-hidden"
+        name="rating"
+        value={rating}
+        checked={+currentRating === rating}
+        id={`${rating}-stars`}
+        type="radio"
         onChange={handleInputChange}
       />
-      <label htmlFor={`${index}-stars`} className="reviews__rating-label form__rating-label" title={ratingInputTitle[index - 1]}>
+      <label htmlFor={`${rating}-stars`} className="reviews__rating-label form__rating-label" title={ratingInputTitle[-rating + 5]}>
         <svg className="form__star-image" width="37" height="33">
           <use xlinkHref="#icon-star"></use>
         </svg>
@@ -23,7 +28,8 @@ const RatingStar = (props) => {
 };
 
 RatingStar.propTypes = {
-  index: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  currentRating: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
 
