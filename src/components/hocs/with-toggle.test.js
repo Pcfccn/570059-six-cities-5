04f37ef9b69b923a65f36cf46608587553e730c9
unknown <kraynevroman@gmail.com>
2020-11-24 23:05:44 +0300

    add snapshot for hoc

---
+++ src/components/hocs/with-toggle.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import PropTypes from "prop-types";
+import withToggle from "./with-toggle/with-toggle";
+
+const MockComponent = (props) => {
+  const {children} = props;
+
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+MockComponent.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+};
+
+const MockComponentWrapped = withToggle(MockComponent);
+
+it(`withToggle is rendered correctly`, () => {
+  const tree = renderer.create((
+    <MockComponentWrapped>
+      <React.Fragment />
+    </MockComponentWrapped>
+  )).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
