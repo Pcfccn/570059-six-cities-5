9e0d9a47db46782cfcd7ba2361be4e46ea2e8538
unknown <kraynevroman@gmail.com>
2020-11-26 13:02:59 +0300

    add actionCreator test

---
+++ src/store/action.test.js
@@ -0,0 +1,98 @@
+import {ActionType, AuthorizationStatus, SortType} from "../constants";
+import {ActionCreator} from "./action";
+
+describe(`Action crator work corectly`, () => {
+  it(`change city`, () => {
+    expect(ActionCreator.changeCity(``)).toEqual({
+      type: ActionType.CHANGE_CITY,
+      payload: ``,
+    });
+  });
+
+  it(`changeSortType`, () => {
+    expect(ActionCreator.changeSortType([1, 2], `Paris`, SortType.POPULAR_DESC)).toEqual({
+      type: ActionType.CHANGE_SORT_TYPE,
+      payload: {
+        offers: [1, 2],
+        city: `Paris`,
+        type: SortType.POPULAR_DESC},
+    });
+  });
+
+  it(`changeEnteredOffer`, () => {
+    expect(ActionCreator.changeEnteredOffer({})).toEqual({
+      type: ActionType.CHANGE_ENTERED_OFFER,
+      payload: {offer: {}},
+    });
+  });
+
+  it(`enterText`, () => {
+    expect(ActionCreator.enterText(`text`)).toEqual({
+      type: ActionType.ENTER_TEXT,
+      payload: `text`,
+    });
+  });
+
+  it(`loadUserData`, () => {
+    expect(ActionCreator.loadUserData({})).toEqual({
+      type: ActionType.LOAD_USER_DATA,
+      payload: {},
+    });
+  });
+
+  it(`loadOffers`, () => {
+    expect(ActionCreator.loadOffers([])).toEqual({
+      type: ActionType.LOAD_OFFERS,
+      payload: [],
+    });
+  });
+
+  it(`loadOneOffer`, () => {
+    expect(ActionCreator.loadOneOffer({})).toEqual({
+      type: ActionType.LOAD_ONE_OFFER,
+      payload: {},
+    });
+  });
+
+  it(`loadComments`, () => {
+    expect(ActionCreator.loadComments({})).toEqual({
+      type: ActionType.LOAD_COMMENTS,
+      payload: {},
+    });
+  });
+
+  it(`loadNearbyOffers`, () => {
+    expect(ActionCreator.loadNearbyOffers([])).toEqual({
+      type: ActionType.LOAD_NEARBY_OFFERS,
+      payload: [],
+    });
+  });
+
+  it(`openSortOptions`, () => {
+    expect(ActionCreator.openSortOptions(true)).toEqual({
+      type: ActionType.OPEN_SORT_OPTIONS,
+      payload: false,
+    });
+  });
+
+  it(`redirectToRoute`, () => {
+    expect(ActionCreator.redirectToRoute(`url`)).toEqual({
+      type: ActionType.REDIRECT_TO_ROUTE,
+      payload: `url`,
+    });
+  });
+
+  it(`requireAuthorization`, () => {
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    });
+  });
+
+  it(`setRating`, () => {
+    expect(ActionCreator.setRating(5)).toEqual({
+      type: ActionType.SET_RATING,
+      payload: 5,
+    });
+  });
+});
