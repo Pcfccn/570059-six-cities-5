a17173747bc869823b615dd72e85ebcc1c734fd1
unknown <kraynevroman@gmail.com>
2020-11-30 17:48:30 +0300

    fix D11

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,45 +1,39 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import {Path, AuthorizationStatus} from "../../constants";
 
-class Header extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const {authStatus, email} = this.props;
-    return (
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link className="header__logo-link" to={Path.MAIN}>
-                <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link className="header__nav-link header__nav-link--profile" to={Path.FAVORITES}>
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">
-                      {authStatus === AuthorizationStatus.AUTH
-                        ? email
-                        : `Sign In`}
-                    </span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+const Header = ({authStatus, email}) => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <Link className="header__logo-link" to={Path.MAIN}>
+              <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+            </Link>
           </div>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item user">
+                <Link className="header__nav-link header__nav-link--profile" to={Path.FAVORITES}>
+                  <div className="header__avatar-wrapper user__avatar-wrapper">
+                  </div>
+                  <span className="header__user-name user__name">
+                    {authStatus === AuthorizationStatus.AUTH
+                      ? email
+                      : `Sign In`}
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
-      </header>
-    );
-  }
-}
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   email: PropTypes.string.isRequired,
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -7,30 +7,22 @@ import MainPlaces from "../main-places/main-places";
 import MainNoPlaces from "../main-no-places/main-no-places";
 import offerPropTypes from "../types/offer";
 
-class Main extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const Main = ({offers, city}) => {
+  return (
+    <div className="page page--gray page--main">
+      <Header />
 
-  render() {
-    const {offers, city} = this.props;
-    return (
-      <div className="page page--gray page--main">
-        <Header />
-
-        <main className="page__main page__main--index">
-          <h1 className="visually-hidden">Cities</h1>
-          <CitiesList />
-          {offers && offers.length
-            ? <MainPlaces city={city}/>
-            : <MainNoPlaces city={city}/>
-          }
-        </main>
-      </div>
-    );
-  }
-}
-
+      <main className="page__main page__main--index">
+        <h1 className="visually-hidden">Cities</h1>
+        <CitiesList />
+        {offers && offers.length
+          ? <MainPlaces city={city}/>
+          : <MainNoPlaces city={city}/>
+        }
+      </main>
+    </div>
+  );
+};
 
 Main.propTypes = {
   offers: PropTypes.arrayOf(offerPropTypes).isRequired,
