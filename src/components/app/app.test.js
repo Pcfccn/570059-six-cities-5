cef5bf1a166c335b4caa2f81b5e223394be6d685
unknown <kraynevroman@gmail.com>
2020-11-30 18:58:39 +0300

    fix B20

---
+++ src/components/app/app.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {Router} from "react-router-dom";
+import App from "./app";
+import browserHistory from "../../brouser-history";
+import {Provider} from "react-redux";
+import toJson from "enzyme-to-json";
+import {configure, mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import configureMockStore from 'redux-mock-store';
+
+configure({adapter: new Adapter()});
+
+let mockState;
+beforeEach(() => {
+  mockState = {
+    USER: {},
+    STATE: {},
+    DATA: {}
+  };
+});
+it(`Should Main render correctly`, () => {
+  const mockStore = configureMockStore();
+  const store = mockStore(mockState);
+
+  const div = global.document.createElement(`div`);
+  global.document.body.appendChild(div);
+
+  const wrapper = mount(
+      <Provider store={store}>
+        <Router history={browserHistory}>
+          <App />
+        </Router>
+      </Provider>,
+      {attachTo: div});
+
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
